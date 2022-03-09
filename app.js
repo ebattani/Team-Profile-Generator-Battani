e6911e2496cb39c4f79e96841f26302c7806b262
Ethan <battaniethan@gmail.com>
2022-03-09 11:38:28 -0700

    fixed issues

--- app.js
+++ app.js
@@ -1,185 +1,208 @@
-// console.log('Hello World');
-
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
-
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
-
-
-console.log("Enter information for each team member");
-console.log("--------------------------------------");
 
 let team = [];
 
-// console.log("You've made it to the team");
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outPut = path.join(OUTPUT_DIR, "team.html");
 
-const questionsForEmployees = [
-  //employee name
+
+const questions = [
+
   {
+
     type: "input",
-    message: "What is the name of the the employee?",
+    message: "What is the employee name?",
     name: "name"
+
   },
-  //employee id
+  
   {
+
     type: "input",
-    message: "What is the id of the employee?",
+    message: "What is the employee ID?",
     name: "id"
+
   },
-  //employee email
+
   {
+
     type: "input",
-    message: "What is the email of the employee?",
+    message: "What is the employee email?",
     name: "email"
+
   },
-  //get roll of employee
+
   {
+
     type: "list",
-    message: "What is the role of the employee?",
+    message: "What is the the employee role?",
     name: "role",
     choices: ["Engineer", "Intern", "Manager"]
+
   }
 ];
 
-// console.log("You've made it past the employee question array");
 
-const questionForEngineer = {
-  // when: choice => {
-  //     return choice.role == "engineer"
-  // },
+const engineerQuestion = {
+
   type: "input",
-  message: "What is your GitHub username?",
-  name: "github"
+  message: "Please enter Github username. No @ necessary.",
+  name: "Github"
+
 };
 
+const internQuestions = {
 
-//when intern
-const questionForIntern = {
-  // when: choice => {
-  //     return choice.role == "intern"
-  // },
   type: "input",
-  message: "What is the name of your school?",
+  message: "Please enter school name.",
   name: "school"
+
 };
 
-// console.log("QFI");
 
-//when manager
-const questionForManager = {
-  // when: choice => {
-  //     return choice.role == "manager"
-  // },
+const managerQuestions = {
+
   type: "input",
-  message: "What is your office number?",
-  name: "officeNumber"
-};
-// console.log("QFM");
+  message: "Please enter office number.",
+  name: "number"
 
-//check if more team members
-const askForMoreEmployees = {
+};
+
+const addEmployees = {
+
   type: "confirm",
   name: "addMore",
-  message: "Do you have any more team members you'd like to add?",
+  message: "Do you have more employees to add?",
   default: false
+
 };
 
-// console.log("check employees");
+async function addEmployeeFun() {
 
-//create async function to check if user added more employees
-async function checkForMoreEmployees() {
   try {
-    const checkForMore = await inquirer.prompt(askForMoreEmployees);
+
+    const checkForMore = await inquirer.prompt(addEmployees);
+
     if (checkForMore.addMore) {
+
       await createTeam();
+
     }
+
     return team;
+    
   } catch (err) {
-    console.log("There is an error in the checkForMoreEmployees Function");
+
+    console.log("There is an error in the addEmployeeFun Function");
+
   }
 }
 
 //create another asyn funtion that creates the team by pushing the new constructor into empty team array
 async function createTeam() {
+
   try {
-    const inputFromUsers = await inquirer.prompt(questionsForEmployees);
-    const { name, id, email } = inputFromUsers;
-    //use switch and break
-    switch (inputFromUsers.role) {
-      //case for engineer
+
+    const userInput = await inquirer.prompt(questions);
+    const { name, id, email } = userInput;
+
+
+    switch (userInput.role) {
+
       case "Engineer":
+
         try {
-          const engineerInput = await inquirer.prompt(questionForEngineer);
+
+          const engineerInput = await inquirer.prompt(engineerQuestion);
           const { github } = engineerInput;
+
           let engineer = new Engineer(name, id, email, github);
+
           team.push(engineer);
-          await checkForMoreEmployees();
+
+          await addEmployeeFun();
+
         } catch (err) {
-          console.log("Error for the engineer");
+
+          console.log("Error");
         }
-        break;
-      //case for intern
+  
       case "Intern":
+
         try {
-          const internInput = await inquirer.prompt(questionForIntern);
+
+          const internInput = await inquirer.prompt(internQuestions);
           const { school } = internInput;
+          
           let intern = new Intern(name, id, email, school);
+
           team.push(intern);
-          await checkForMoreEmployees();
+
+          await addEmployeeFun();
+
         } catch (err) {
-          console.log("Error for the intern");
+
+          console.log("Error");
+
         }
+        
         break;
-      //case for manager
+
       case "Manager":
+
         try {
-          const managerInput = await inquirer.prompt(questionForManager);
-          const { officeNumber } = managerInput;
-          let manager = new Manager(name, id, email, officeNumber);
+
+          const managerInput = await inquirer.prompt(managerQuestions);
+          const { number } = managerInput;
+
+          let manager = new Manager(name, id, email, number);
+
           team.push(manager);
-          await checkForMoreEmployees();
+
+          await addEmployeeFun();
+
         } catch (err) {
-          console.log("Error for the manager");
+
+          console.log("Error");
+
         }
+
         break;
     }
-    //catch
+
   } catch (err) {
+
     console.log("Error!");
   }
 }
 
-// console.log("wow, much far");
-//async function
 async function mainHTML() {
-  //waits til createTeam function is ready
+
   await createTeam();
 
-
-//check if file does not exist by checking with fs
   if (!fs.existsSync("./output")) {
-    console.log("outputting");
-    //if does not exit - create directory folder 'output'
+
     fs.mkdirSync("./output");
+
   }
   //write to file
-  //use parameters var outputPath, the render team function passing
+  //use parameters var outPut, the render team function passing
   //the array team, and the anon callback function err
-  fs.writeFile(outputPath,  render(team), error => {
+  fs.writeFile(outPut,  render(team), error => {
+
     if (error) throw error;
-    console.log("You have successfully generated your new team");
-    console.log("---------------------------------------------");
+
   });
 }
+
 //call mainHTML function and return promise/ catch
 mainHTML()
+
   .then(() => console.log("Please check the output folder for your team.html"))
   .catch(error => console.log(error));
